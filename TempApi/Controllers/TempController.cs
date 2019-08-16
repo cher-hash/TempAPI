677d2a43eb9f371781906f22e0f7177b305c6d09
cher-hash <cherish34@gmail.com>
2019-08-15 21:33:09 -0600

    Initial commit

---
+++ TempApi/Controllers/TempController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TempApi.Models;
+using TempApi.Services;
+
+namespace TempApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TempController : ControllerBase
+    {
+        private readonly TempService _tempService;
+
+        public TempController(TempService tempService)
+        {
+            _tempService = tempService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<TempItem>> Get() => _tempService.Get();
+
+
+        [HttpGet("{id:length(24)}", Name = "GetTemp")]
+        public ActionResult<TempItem> Get(string id)
+        {
+            var temp = _tempService.Get(id);
+
+            if (temp == null)
+            {
+                return NotFound();
+            }
+
+            return temp;
+        }
+
+        [HttpPost]
+        public ActionResult<TempItem> Create(TempItem temp)
+        {
+            _tempService.Create(temp);
+
+            return CreatedAtRoute("GetTemp", new { id = temp.Id.ToString() }, temp);
+        }
+
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, TempItem tempIn)
+        {
+            var temp = _tempService.Get(id);
+
+            if (temp == null)
+            {
+                return NotFound();
+            }
+
+            _tempService.Update(id, tempIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var temp = _tempService.Get(id);
+
+            if (temp == null)
+            {
+                return NotFound();
+            }
+
+            _tempService.Remove(temp.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ TempApi/Models/TempItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace TempApi.Models
+{
+    public class TempItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string SensorName { get; set; }
+        public DateTime Timestamp { get; set; }
+        public decimal Temperature { get; set; }
+        public string HeaterAction { get; set; }
+    }
+
+}
